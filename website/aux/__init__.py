82507def1e3dd64a8d75d981d2d32448f86e1957
Varaun Ramgoolie <varaunramgoolie@hotmail.com>
2023-06-07 02:25:15 -0400

    working on debugging relative import

--- website/aux/__init__.py
+++ website/aux/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, static_folder="static", static_url_path="/static")
     app.config['SECRET_KEY'] = 'hellohello'
 
     from .views import views
--- website/aux/views.py
+++ website/aux/views.py
@@ -1,8 +1,20 @@
-from flask import Blueprint, render_template, url_for
+from flask import Blueprint, render_template, url_for, request
 from flask_login import current_user
+from ...program.nodes import *
+
 
 views = Blueprint("views", __name__)
 
-@views.route("/")
+@views.route("/", methods=["POST", "GET"])
 def home():
-    return render_template("home.html")
+    if request.method == "POST":
+        start = request.form["from"]
+        end = request.form["to"]
+        return render_template("home.html", start = start, end = end, objects = objects)
+    else:
+        return render_template("home.html")
+
+@views.route("/about")
+def about():
+    return render_template("about.html")
+
