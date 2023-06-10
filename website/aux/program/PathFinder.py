bfb04d81d0f35ee1a93192e781f65f61209ad14f
Varaun Ramgoolie <varaunramgoolie@hotmail.com>
2023-06-10 02:44:24 -0400

    Remove unnecessary code in PathFinder.py

--- website/aux/program/PathFinder.py
+++ website/aux/program/PathFinder.py
@@ -48,77 +48,7 @@ def make_dir(lon):
         lon.pop(0)
 
     return directions
-
-
-x=10
-def prints(text):
-    if x == 1:
-        for char in text:
-            print(char, end='',flush=True)
-            time.sleep(random.uniform(0,0.1))
-    else:
-        print(text)
-
-def cursor():
-    time.sleep(0.3)
-
     
-
-def all_paths(node1, node2):
-    queue = [node1]
-    path = [node1]
-    paths = []
-
-    find_paths(node1, node2, queue, path, paths)
-
-    namedpaths = []
-    for path in paths:
-        namedpaths.append(nodename(path))
-
-    if len(namedpaths) == 0:
-        prints("It seems that there are no completely internal paths from " + node1.name + " to " + node2.name + ". Brace for the weather traveler.")
-        return
-    else: 
-        prints("The possible paths from "+node1.name+" to "+node2.name+" are: ")
-        print()
-        for path in namedpaths:
-            prints(str(namedpaths.index(path) + 1) + "." + str(path))
-            print()
-            cursor()
-
-    prints("Choose your preferred path by selecting a number from 1 to " + str(len(namedpaths)) + ", or press 'q' to exit: ")
-    choice = input()
-    if choice.upper() == 'Q':
-        prints("Have a nice day!")
-        return
-    else:
-        prints("Okay!")
-        print()
-        time.sleep(1)
-        prints("*ehem*")
-        print()
-        time.sleep(1)
-        prints("Here we go!")
-        print()
-        time.sleep(1)
-        prints("...drumroll please...")
-        print()
-        print()
-        time.sleep(1)
-
-        prints("Starting at "+node1.name+": ")
-        print()
-        directions = make_dir(paths[(int(choice) - 1)])
-        i = 0
-        cur_path = namedpaths[(int(choice)-1)]
-        for dir in directions:
-            prints(str(directions.index(dir) + 1)+ ": ["+ cur_path[i] +"] ---> " + dir + ". ---> ["+cur_path[i+1]+"]")
-            i+=1
-            cursor()
-            print()
-        prints("You've arrived at "+cur_path[-1]+"! Big ups! Enjoy your wadeva. Laters :D")
-
-
 
 # Takes two nodes and returns a list (of lists) all the paths between these nodes,
 # and then calls nodename on all the nodes in the paths
